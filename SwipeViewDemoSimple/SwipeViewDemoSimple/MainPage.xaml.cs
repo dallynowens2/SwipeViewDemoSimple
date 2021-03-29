0bf22ecb29c06931b6e11b25ff5b6c981ff99e58
dallynowens2 <dallynowens2@gmail.com>
2021-03-29 15:16:23 -0600

    Add project files.

---
+++ SwipeViewDemoSimple/SwipeViewDemoSimple/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SwipeViewDemoSimple
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnFavoriteSwipeItemInvoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SwipeView", "Favorite invoked.", "OK");
+        }
+
+        async void OnShareSwipeItemInvoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SwipeView", "Share invoked.", "OK");
+        }
+
+        async void OnDeleteSwipeItemInvoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SwipeView", "Delete invoked.", "OK");
+        }
+    }
+}
